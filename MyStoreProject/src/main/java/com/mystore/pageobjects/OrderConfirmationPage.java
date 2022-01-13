28e75153bf6db375a4a97576fc00f635f2d888dd
Kolagatla Madhavi Jyothi <madhavi.jyothi@FLYSPICE.COM>
2022-01-13 23:37:55 +0530

    store project

---
+++ MyStoreProject/src/main/java/com/mystore/pageobjects/OrderConfirmationPage.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.mystore.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.mystore.actiondriver.Action;
+import com.mystore.base.BaseClass;
+
+/**
+ * @author Hitendra
+ *
+ */
+public class OrderConfirmationPage extends BaseClass {
+	
+	Action action= new Action();
+	
+	@FindBy(xpath="//p/strong[contains(text(),'Your order on My Store is complete.')]")
+	private WebElement confirmMessag;
+	
+	public OrderConfirmationPage() {
+		PageFactory.initElements(getDriver(), this);
+	}
+	
+	public String validateConfirmMessage() {
+		String confirmMsg=confirmMessag.getText();
+		return confirmMsg;
+	}
+
+	
+}
